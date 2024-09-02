217c99c0497e955e860618033d5b1bf1f4b40671
Florent Charpentier <florent.charpentier@epitech.eu>
2024-09-02 14:33:13 +0200

    :sparkles: Added bash aliases

--- shell/bash.nix
+++ shell/bash.nix
@@ -7,6 +7,22 @@
       cfgedit = "cfg \"sudo --preserve-env=SHLVL su\"";
 
       "²" = "true";
+
+      ga = "git add";
+      gcm = "git commit -m";
+      gcam = "git commit -am";
+      gp = "git push";
+      gpl = "git pull";
+      gcl = "git clone";
+      gs = "git status";
+      gr = "git restore";
+      grs = "git restore --staged";
+      gst = "git stash push";
+      gstp = "git stash pop";
+      gl = "git log";
+      gpf = "git push --force-with-lease";
+      gpff = "git push --force";
+      gd = "git diff";
     };
 
     bashrcExtra = ''

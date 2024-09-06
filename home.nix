b0f1705a51e907aa0fc2e838042064f5bcd985ee
Florent Charpentier <florent.charpentier@epitech.eu>
2024-09-06 11:12:22 +0200

    :sparkles: Added postman package

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "vscode"
+    "postman"
   ];
 
   imports = [
@@ -29,23 +30,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
     tree
+    postman
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage

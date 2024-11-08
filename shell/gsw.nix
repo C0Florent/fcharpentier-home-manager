e823b208425cf59de2c6ed6a9d0bd66c149aa006
Florent Charpentier <florent.charpentier@epitech.eu>
2024-11-08 13:45:55 +0100

    :sparkles: shell: Added text hint to `gsw`

--- shell/gsw.nix
+++ shell/gsw.nix
@@ -14,10 +14,16 @@ pkgs.writeShellScriptBin "gsw" ''
     if [ -z $current_branch ]; then
         exit 1;
     fi
+
+    echo
+    echo 'Select a branch with fzf, or press ESC or ^C to exit'
     
     chosen_branch="$((echo "$current_branch"; git branch | grep -v "^\* $current_branch\$") | ${fzf} '--height=~50%')"
 
+    printf '\e[A\e[K'
+
     if [ -z "$chosen_branch" ]; then
+        printf '\e[A\e[K'
         exit 1;
     fi
     

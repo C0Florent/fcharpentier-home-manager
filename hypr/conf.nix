99aae6b64fd70e88b40ecf53cd806338db4bbe71
Florent Charpentier <florent.charpentier@epitech.eu>
2025-02-03 20:42:10 +0100

    :sparkles: hypr: Enabled brightness and audio keys

--- hypr/conf.nix
+++ hypr/conf.nix
@@ -1,13 +1,17 @@
 { pkgs, lib, config, ... }:
 
 let
-  terminal = "${pkgs.alacritty}/bin/alacritty";
-  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
   lmb = "mouse:272";
   rmb = "mouse:273";
 in
 
 {
+  home.packages = with pkgs; [
+    brightnessctl
+    wireplumber
+    playerctl
+  ];
+
   wayland.windowManager.hyprland = {
     settings = {
       "$mainMod" = "SUPER";
@@ -51,6 +55,21 @@ in
         "$mainMod, C, killactive"
 
         "$mainMod + CTRL, X, exec, uwsm stop"
+      ];
+
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", XF86MonBrightnessUp, exec, brightnessctl s 5%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+      ];
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
       ];
 
       input = {
